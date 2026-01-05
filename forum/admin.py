8eb25663b0b31b7d5c9ec4365219ea2fe6c535e4
Prann <prannvijaysingh6307@gmail.com>
2026-01-06 00:05:04 +0530

    fixing issues

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Course, Resource, Category, Thread, Reply
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Course)
+admin.site.register(Resource)
+admin.site.register(Thread)
+admin.site.register(Reply)
