a4d575432cb5cd2dba6085db211b1c9826f00073
Prann <prannvijaysingh6307@gmail.com>
2026-01-05 22:52:49 +0530

    push

---
+++ forum/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+
+class Course(models.Model):
+    code = models.CharField(max_length=20, unique=True) 
+    title = models.CharField(max_length=200) 
+    department = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.code}: {self.title}"
+
+class Resource(models.Model):
+    RESOURCE_TYPES = [('PDF', 'PDF'), ('VIDEO', 'Video'), ('LINK', 'Link')]
+    
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    resource_type = models.CharField(max_length=10, choices=RESOURCE_TYPES)
+    link = models.URLField()
+
+    def __str__(self):
+        return self.title
+    
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+class Thread(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='threads')
+    course_tag = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    likes = models.ManyToManyField(User, related_name='thread_likes', blank=True)
+
+    def total_likes(self):
+        return self.likes.count()
+
+class Reply(models.Model):
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE, related_name='replies')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Reply by {self.author.username} on {self.thread.title}"
---
+++ forum/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Thread, Reply, Category
+from django.contrib.postgres.search import TrigramSimilarity
+
+def home(request):
+    sort_by = request.GET.get('sort', 'latest')
+    if sort_by == 'popular':
+        threads = sorted(Thread.objects.all(), key=lambda t: t.total_likes(), reverse=True)
+    else:
+        threads = Thread.objects.all().order_by('-created_at')
+    
+    return render(request, 'forum/home.html', {'threads': threads})
+
+@login_required
+def thread_detail(request, thread_id):
+    thread = get_object_or_404(Thread, pk=thread_id)
+    replies = thread.replies.filter(is_deleted=False)
+    
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        Reply.objects.create(thread=thread, author=request.user, content=content)
+        return redirect('thread_detail', thread_id=thread.id)
+
+    return render(request, 'forum/thread_detail.html', {'thread': thread, 'replies': replies})
+
+def search(request):
+    query = request.GET.get('q')
+    results = Thread.objects.annotate(
+        similarity=TrigramSimilarity('title', query)
+    ).filter(similarity__gt=0.3).order_by('-similarity')
+    
+@login_required
+def delete_reply(request, reply_id):
+    reply = get_object_or_404(Reply, pk=reply_id)
+    if request.user == reply.author or request.user.is_staff:
+        reply.is_deleted = True
+        reply.save()
+    return redirect('thread_detail', thread_id=reply.thread.id)
