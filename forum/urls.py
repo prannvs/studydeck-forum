8f7b18a128a2f9d2eeef6959d59c2ff7599f942d
Prann <prannvijaysingh6307@gmail.com>
2026-01-28 02:36:38 +0530

    Fix

--- forum/urls.py
+++ forum/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('search/', views.search, name='search'),
     path('thread/<slug:slug>/', views.thread_detail, name='thread_detail'),
     path('create/', views.create_thread, name='create_thread'),
     path('profile/<str:username>/', views.user_profile, name='user_profile'),
