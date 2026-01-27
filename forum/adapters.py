5e9d8e6e309ba5d829f1b02c97036867f1d8a0fb
Prann <prannvijaysingh6307@gmail.com>
2026-01-28 03:02:39 +0530

    Fix

--- forum/adapters.py
+++ forum/adapters.py
@@ -1,10 +1,23 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-from django.forms import ValidationError
+from django.contrib import messages
+from django.shortcuts import redirect
+from allauth.core.exceptions import ImmediateHttpResponse
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
+        if not sociallogin.user.email:
+             return
+             
         email = sociallogin.user.email
+        
         allowed_domains = ['bits-pilani.ac.in', 'pilani.bits-pilani.ac.in'] 
-        domain = email.split('@')[1]
+        
+        try:
+            domain = email.split('@')[1]
+        except IndexError:
+            messages.error(request, "Invalid email address.")
+            raise ImmediateHttpResponse(redirect('account_login'))
+
         if not any(domain.endswith(d) for d in allowed_domains):
-            raise ValidationError("Only BITS Pilani email addresses are allowed.")
+            messages.error(request, f"Access Denied: The email '{email}' is not a BITS Pilani address.")
+            raise ImmediateHttpResponse(redirect('account_login'))
